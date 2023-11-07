4b49e3e1dc2220ba2354d27d5ea62e25341c1eb8
hahongha <phamha03122003@gmail.com>
2023-11-08 00:41:36 +0700

    first commit

---
+++ OnKT_Lab4/Models/MenuItem.cs
@@ -0,0 +1,10 @@
+﻿namespace OnKT_Lab4.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Link { get; set; }
+    }
+}
---
+++ OnKT_Lab4/ViewComponents/MajorViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnKT_Lab4.Data;
+using OnKT_Lab4.Models;
+
+namespace OnKT_Lab4.ViewComponents
+{
+    public class MajorViewComponent: ViewComponent
+    {
+        SchoolContext db;
+        List<Major> majors;
+        public MajorViewComponent(SchoolContext context)
+        {
+            db = context;
+            majors = db.Majors.ToList();
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("RenderMajor", majors);
+        }
+    }
+}
